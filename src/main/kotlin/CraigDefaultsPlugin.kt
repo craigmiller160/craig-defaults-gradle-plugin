46bcfc5cc22cb3256baa5c1126e96e16fbb53670
Craig Miller <craigmiller160@gmail.com>
2023-03-05 13:44:38 -0500

    Cleanup

--- src/main/kotlin/CraigDefaultsPlugin.kt
+++ src/main/kotlin/CraigDefaultsPlugin.kt
@@ -1,9 +1,8 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-class HelloPlugin : Plugin<Project> {
+class CraigDefaultsPlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        kotlin.runCatching {  }.onFailure {  }
         TODO("Not yet implemented")
     }
 }
