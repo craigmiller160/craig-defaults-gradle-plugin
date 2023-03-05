441c8460c7a0583c04fb5460aab2a9e783b75500
Craig Miller <craigmiller160@gmail.com>
2023-03-05 13:49:49 -0500

    More plugin work

--- src/main/kotlin/io/craigmiller160/gradle/plugins/CraigDefaultsPlugin.kt
+++ src/main/kotlin/io/craigmiller160/gradle/plugins/CraigDefaultsPlugin.kt
@@ -1,27 +1,11 @@
 package io.craigmiller160.gradle.plugins
 
+import io.craigmiller160.gradle.plugins.githooks.createDefaultGitHooksTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.nio.file.StandardCopyOption
 
 class CraigDefaultsPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        project.tasks.register("installDefaultGitHooks") { task ->
-            task.doLast { taskDoLast ->
-                taskDoLast.logger.info("Installing Default Git Hooks")
-                val hooksSourceDir = Paths.get(System.getProperty("user.home"), ".gradle", "gitHooks")
-                val hooksTargetDir = Paths.get(project.rootDir.absolutePath, ".git", "hooks")
-                runCatching {
-                    Files.list(hooksSourceDir)
-                        .forEach { hookPath ->
-                            val hookGitPath = hooksTargetDir.resolve(hookPath.fileName)
-                            Files.copy(hookPath, hookGitPath, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES)
-                        }
-                }
-                    .onFailure { it.printStackTrace() }
-            }
-        }
+        project.createDefaultGitHooksTask()
     }
 }
