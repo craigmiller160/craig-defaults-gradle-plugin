7acd737dacf8f922a8f687c8d00ffc71c9917219
Craig Miller <craigmiller160@gmail.com>
2023-03-05 14:08:34 -0500

    More progress with build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,32 @@ val projectVersion: String by project
 
 plugins {
     kotlin("jvm")
+    `maven-publish`
 }
 
 group = projectGroup
 version = projectVersion
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = rootProject.name
+            version = project.version.toString()
+
+            from(components["kotlin"])
+        }
+    }
+    repositories {
+        maven {
+            val repo = if (project.version.toString().endsWith("-SNAPSHOT")) "maven-snapshots" else "maven-releases"
+            url = uri("https://nexus-craigmiller160.ddns.net/repository/$repo")
+            credentials {
+                username = System.getenv("NEXUS_USER")
+                password = System.getenv("NEXUS_PASSWORD")
+            }
+        }
+    }
+}
 
 dependencies {
     testImplementation(kotlin("test"))
