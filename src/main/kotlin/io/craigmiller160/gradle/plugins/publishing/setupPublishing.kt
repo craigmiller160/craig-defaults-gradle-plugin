10dffcf2e6879784ca3eff5aafcc8f256262301b
Craig Miller <craigmiller160@gmail.com>
2023-03-06 17:03:50 -0500

    Updated setupPublishing

--- src/main/kotlin/io/craigmiller160/gradle/plugins/publishing/setupPublishing.kt
+++ src/main/kotlin/io/craigmiller160/gradle/plugins/publishing/setupPublishing.kt
@@ -1,10 +1,22 @@
 package io.craigmiller160.gradle.plugins.publishing
 
 import org.gradle.api.Project
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPublication
 
 fun Project.setupPublishing() {
     pluginManager.withPlugin("maven-publish") {
-        println("WORKING")
+        extensions.configure<PublishingExtension>("publishing") { publishing ->
+            publishing.publications { publication ->
+                publication.create("maven") {
+                    val pub = it as MavenPublication
+                    pub.groupId = project.group.toString()
+                    pub.artifactId = rootProject.name
+                    pub.version = project.version.toString()
+
+                    pub.from(components.getByName("kotlin"))
+                }
+            }
+        }
     }
-//    println("PLUGIN: ${plugins.findPlugin("org.gradle.maven-publish")}")
 }
