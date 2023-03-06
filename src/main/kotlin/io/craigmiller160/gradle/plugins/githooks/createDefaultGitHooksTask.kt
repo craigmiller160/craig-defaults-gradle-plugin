ff0966c7f9ebd7bc7c1746580722a3e1724f4609
Craig Miller <craigmiller160@gmail.com>
2023-03-06 07:42:35 -0500

    Cleaned up tasks

--- src/main/kotlin/io/craigmiller160/gradle/plugins/githooks/createDefaultGitHooksTask.kt
+++ src/main/kotlin/io/craigmiller160/gradle/plugins/githooks/createDefaultGitHooksTask.kt
@@ -9,17 +9,9 @@ fun Project.createDefaultGitHooksTask() {
     tasks.register("installDefaultGitHooks") { task ->
         task.doLast { taskDoLast ->
             taskDoLast.logger.info("Installing Default Git Hooks")
-            // TODO heavily refactor this logic
-            val hooksSourceDir = Paths.get(System.getProperty("user.home"), ".gradle", "gitHooks")
-            val hooksTargetDir = Paths.get(rootDir.absolutePath, ".git", "hooks")
-            runCatching {
-                Files.list(hooksSourceDir)
-                    .forEach { hookPath ->
-                        val hookGitPath = hooksTargetDir.resolve(hookPath.fileName)
-                        Files.copy(hookPath, hookGitPath, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES)
-                    }
-            }
-                .onFailure { it.printStackTrace() }
+            val sourcePath = Paths.get(ClassLoader.getSystemClassLoader().getResource("githooks/pre-commit").toURI())
+            val targetPath = Paths.get(System.getProperty("user.dir"), ".git", "hooks", "pre-commit")
+            Files.copy(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES)
         }
     }
 }
