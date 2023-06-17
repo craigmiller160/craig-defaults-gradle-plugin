f1c700de9a43da28ee89490adddcfd345368d585
Craig Miller <craigmiller160@gmail.com>
2023-06-17 16:13:24 -0400

    Squashed commit of the following:
    commit ee53164b7f931dcf96c4aabd7e382bfd880a34f1
    Author: Craig Miller <craigmiller160@gmail.com>
    Date:   Sat Jun 17 16:13:00 2023 -0400
    
        Fixed remaining jar task stuff
    
    commit a782d9c4c3a78e10d57e783f976e4f7a97e3ee2d
    Author: Craig Miller <craigmiller160@gmail.com>
    Date:   Sat Jun 17 16:09:54 2023 -0400
    
        Deleting plain jars
    
    commit afef7f979d8d8ce778fa90532701dff38858c403
    Author: Craig Miller <craigmiller160@gmail.com>
    Date:   Sat Jun 17 16:05:15 2023 -0400
    
        Fixed version
    
    commit b6ba1fbc1a838151ea86248b718593efe5c54a60
    Author: Craig Miller <craigmiller160@gmail.com>
    Date:   Sat Jun 17 16:03:14 2023 -0400
    
        Giving better variable names

--- src/main/kotlin/io/craigmiller160/gradle/plugins/building/fixJarForSpring.kt
+++ src/main/kotlin/io/craigmiller160/gradle/plugins/building/fixJarForSpring.kt
@@ -1,14 +1,12 @@
 package io.craigmiller160.gradle.plugins.building
 
 import org.gradle.api.Project
-import org.gradle.jvm.tasks.Jar
 
 fun Project.fixJarForSpring() {
   afterEvaluate { project ->
     project.tasks.findByName("bootJar")?.let {
-      project.tasks.getByName("jar") {
-        val task = it as Jar
-        task.archiveClassifier.set("")
+      project.tasks.getByName("jar") { rawJarTask ->
+        rawJarTask.enabled = false
       }
     }
   }
