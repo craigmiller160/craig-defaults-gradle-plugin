a782d9c4c3a78e10d57e783f976e4f7a97e3ee2d
Craig Miller <craigmiller160@gmail.com>
2023-06-17 16:09:54 -0400

    Deleting plain jars

--- src/main/kotlin/io/craigmiller160/gradle/plugins/building/fixJarForSpring.kt
+++ src/main/kotlin/io/craigmiller160/gradle/plugins/building/fixJarForSpring.kt
@@ -2,15 +2,22 @@ package io.craigmiller160.gradle.plugins.building
 
 import org.gradle.api.Project
 import org.gradle.jvm.tasks.Jar
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.io.path.name
 
 fun Project.fixJarForSpring() {
   afterEvaluate { project ->
     project.tasks.findByName("bootJar")?.let { rawBootJarTask ->
-      val bootJarTask = rawBootJarTask as Jar;
 
-      project.tasks.getByName("jar") { rawJarTask ->
+    project.tasks.getByName("jar") { rawJarTask ->
         val jarTask = rawJarTask as Jar
-        jarTask.archiveClassifier.set("")
+          jarTask.doLast {
+              val libsDir = Paths.get(System.getProperty("user.dir"), "build", "libs")
+              Files.list(libsDir)
+                  .filter { file -> file.name.endsWith("-plain.jar") }
+                  .forEach { Files.delete(it) }
+          }
       }
     }
   }
