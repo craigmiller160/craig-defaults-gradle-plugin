ee53164b7f931dcf96c4aabd7e382bfd880a34f1
Craig Miller <craigmiller160@gmail.com>
2023-06-17 16:13:00 -0400

    Fixed remaining jar task stuff

--- src/main/kotlin/io/craigmiller160/gradle/plugins/building/fixJarForSpring.kt
+++ src/main/kotlin/io/craigmiller160/gradle/plugins/building/fixJarForSpring.kt
@@ -1,23 +1,12 @@
 package io.craigmiller160.gradle.plugins.building
 
 import org.gradle.api.Project
-import org.gradle.jvm.tasks.Jar
-import java.nio.file.Files
-import java.nio.file.Paths
-import kotlin.io.path.name
 
 fun Project.fixJarForSpring() {
   afterEvaluate { project ->
-    project.tasks.findByName("bootJar")?.let { rawBootJarTask ->
-
-    project.tasks.getByName("jar") { rawJarTask ->
-        val jarTask = rawJarTask as Jar
-          jarTask.doLast {
-              val libsDir = Paths.get(System.getProperty("user.dir"), "build", "libs")
-              Files.list(libsDir)
-                  .filter { file -> file.name.endsWith("-plain.jar") }
-                  .forEach { Files.delete(it) }
-          }
+    project.tasks.findByName("bootJar")?.let {
+      project.tasks.getByName("jar") { rawJarTask ->
+        rawJarTask.enabled = false
       }
     }
   }
