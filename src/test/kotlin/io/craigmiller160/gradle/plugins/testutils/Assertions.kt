f0c876a5c3401c81e13d4e4b05aca001f76659dc
Craig Miller <craigmiller160@gmail.com>
2023-07-02 13:16:20 -0400

    Progress

--- src/test/kotlin/io/craigmiller160/gradle/plugins/testutils/Assertions.kt
+++ src/test/kotlin/io/craigmiller160/gradle/plugins/testutils/Assertions.kt
@@ -1,10 +1,12 @@
 package io.craigmiller160.gradle.plugins.testutils
 
 import io.kotest.matchers.collections.shouldContainInOrder
+import io.kotest.matchers.collections.shouldHaveSize
 import org.gradle.testkit.runner.BuildTask
 
 fun List<BuildTask>.shouldHaveExecuted(vararg tasks: BuildTask) {
     val expectedTaskStrings = tasks.map { it.toString() }
     val actualTaskStrings = this.map { it.toString() }
+    actualTaskStrings.shouldHaveSize(expectedTaskStrings.size)
     actualTaskStrings.shouldContainInOrder(expectedTaskStrings)
 }
