add4ab3eb188179e6404bb9ec8681cb878582c1b
Craig Miller <craigmiller160@gmail.com>
2023-07-02 13:20:02 -0400

    Stubbed out new tests

---
+++ src/test/kotlin/io/craigmiller160/gradle/plugins/githooks/SetupSpotlessGitHookTest.kt
@@ -0,0 +1,20 @@
+package io.craigmiller160.gradle.plugins.githooks
+
+import org.junit.jupiter.api.Test
+
+class SetupSpotlessGitHookTest {
+    @Test
+    fun `does nothing when spotless plugin is not present`() {
+        TODO()
+    }
+
+    @Test
+    fun `does nothing when spotless plugin is present but guard file is also present`() {
+        TODO()
+    }
+
+    @Test
+    fun `writes pre-commit git hook when spotless plugin is present`() {
+        TODO()
+    }
+}
