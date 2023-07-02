ab85411cf894d0d694036261c73ea3133d640108
Craig Miller <craigmiller160@gmail.com>
2023-07-02 12:47:02 -0400

    Working on first test

--- src/test/kotlin/io/craigmiller160/gradle/plugins/FixJarForSpringTest.kt
+++ src/test/kotlin/io/craigmiller160/gradle/plugins/FixJarForSpringTest.kt
@@ -1,10 +1,29 @@
 package io.craigmiller160.gradle.plugins
 
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import java.io.File
+import java.lang.RuntimeException
+import java.nio.file.Files
 
 class FixJarForSpringTest {
+    private lateinit var runner: GradleRunner
+
+    @BeforeEach
+    fun setup(@TempDir tempDir: File) {
+        runner = GradleRunner.create()
+            .withPluginClasspath()
+            .withProjectDir(tempDir)
+
+        if (!File(tempDir, "build.gradle.kts").createNewFile()) {
+            throw RuntimeException("Cannot create temporary gradle build file")
+        }
+    }
     @Test
     fun experiment() {
-        TODO()
+        val result = runner.withArguments("tasks").build()
+        println(result.output)
     }
 }
